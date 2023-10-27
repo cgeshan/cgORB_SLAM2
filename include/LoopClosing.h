5fdb5c03781ff3a5a77a0d17bd0d361b15a25ee2
cgeshan <cgeshan@gmail.com>
2023-10-26 21:47:03 -0400

    2023 build changes

--- include/LoopClosing.h
+++ include/LoopClosing.h
@@ -45,7 +45,7 @@ namespace ORB_SLAM2
     public:
         typedef pair<set<KeyFrame *>, int> ConsistentGroup;
         typedef map<KeyFrame *, g2o::Sim3, std::less<KeyFrame *>,
-                    Eigen::aligned_allocator<std::pair<const KeyFrame *, g2o::Sim3>>>
+                    Eigen::aligned_allocator<std::pair<KeyFrame *const, g2o::Sim3>>>
             KeyFrameAndPose;
 
     public:
