81be1efc9e271f752e23ac0f44b779c70525ebce
cgeshan <cgeshan@gmail.com>
2023-12-03 14:19:12 -0500

    Updated Custom

--- Custom/Custom.cpp
+++ Custom/Custom.cpp
@@ -1,189 +1,104 @@
-// #include <algorithm>
-// #include <chrono>
+#include <algorithm>
+#include <chrono>
 
-// #include <opencv2/core/core.hpp>
-// #include <opencv2/opencv.hpp>
+#include <opencv2/core/core.hpp>
+#include <opencv2/opencv.hpp>
 
-// #include <System.h>
+#include <System.h>
 
-// #include "DataStream.h"
-// #include "PNGHandler.h"
+#include "DataStream.h"
+#include "PNGHandler.h"
 
 int main(int argc, char **argv)
 {
-    //     DataStream stream;
-    //     stream.SetFileName("Custom/data_stream");
-    //     stream.SetPermission(0666);
+    DataStream stream;
+    stream.SetFileName("Custom/data_stream");
+    stream.SetPermission(0666);
 
-    //     PNGHandler imgDepth;
-    //     imgDepth.SetResolution(640, 480, 1);
+    const std::string strSettingsFile = argv[2];
+    cv::FileStorage fsSettings(strSettingsFile.c_str(), cv::FileStorage::READ);
+    if (!fsSettings.isOpened())
+    {
+        cerr << "Failed to open settings file at: " << strSettingsFile << endl;
+        exit(-1);
+    }
 
-    //     if (!stream.Init())
-    //     {
-    //         std::cout << "*** ERROR *** Terminating program, pipe initialization failed." << std::endl;
-    //         return 1;
-    //     }
+    PNGHandler imgDepth;
+    // Get the camera resolution from the yaml settings file.cv::FileStorage fSettings(argv[2], cv::FileStorage::READ);
+    imgDepth.SetResolution(fsSettings["Camera.width"], fsSettings["Camera.height"], fsSettings["Camera.channels"]);
 
-    //     stream.OpenReadOnly();
+    if (!stream.Init())
+    {
+        std::cout << "*** ERROR *** Terminating program, pipe initialization failed." << std::endl;
+        return 1;
+    }
 
-    //     if (stream.fileDescriptor == -1)
-    //     {
-    //         std::cerr << "Failed to open the named pipe." << std::endl;
-    //         return 1;
-    //     }
+    stream.OpenReadOnly();
 
-    //     // Buffer reading
-    //     std::vector<char> buffer;
-    //     size_t totalBytes = imgDepth.GetSize() * 5;
-    //     char tempBuffer[totalBytes];
+    if (stream.fileDescriptor == -1)
+    {
+        std::cerr << "Failed to open the named pipe." << std::endl;
+        return 1;
+    }
 
-    //     size_t bytesRead;
+    // Buffer reading
+    std::vector<char> buffer;
+    size_t totalBytes = imgDepth.GetSize() * 5;
+    char tempBuffer[totalBytes];
 
-    //     // Timing
-    //     std::vector<float> vElapsedTime;
+    size_t bytesRead;
 
-    //     while (!stream.terminate)
-    //     {
-    //         bytesRead = read(stream.fileDescriptor, tempBuffer, totalBytes);
-    //         buffer.insert(buffer.end(), tempBuffer, tempBuffer + bytesRead);
-    //         totalBytes -= bytesRead;
+    // Timing
+    std::vector<float> vElapsedTime;
 
-    //         bytesRead = read(stream.fileDescriptor, tempBuffer, totalBytes);
-    //         buffer.insert(buffer.end(), tempBuffer, tempBuffer + bytesRead);
-    //         totalBytes -= bytesRead;
-    //         if (0 == totalBytes)
-    //         {
-    //             std::chrono::high_resolution_clock::time_point currentTime = std::chrono::high_resolution_clock::now();
-    //             std::time_t timestamp = std::chrono::duration_cast<std::chrono::milliseconds>(currentTime.time_since_epoch()).count();
+    while (!stream.terminate)
+    {
+        bytesRead = read(stream.fileDescriptor, tempBuffer, totalBytes);
+        buffer.insert(buffer.end(), tempBuffer, tempBuffer + bytesRead);
+        totalBytes -= bytesRead;
 
-    //             std::vector<char> depthBuffer(buffer.begin(), buffer.end());
-    //             imgDepth.SetFilename("Custom/depth/" + std::to_string(timestamp) + ".png");
-    //             imgDepth.SetData(depthBuffer);
-    //             imgDepth.Save();
+        bytesRead = read(stream.fileDescriptor, tempBuffer, totalBytes);
+        buffer.insert(buffer.end(), tempBuffer, tempBuffer + bytesRead);
+        totalBytes -= bytesRead;
+        if (0 == totalBytes)
+        {
+            std::chrono::high_resolution_clock::time_point currentTime = std::chrono::high_resolution_clock::now();
+            std::time_t timestamp = std::chrono::duration_cast<std::chrono::milliseconds>(currentTime.time_since_epoch()).count();
 
-    //             depthBuffer.clear();
-    //             buffer.clear();
-    //             totalBytes = imgDepth.GetSize();
-    //             std::cout << "Exiting saving loop..." << std::endl;
-    //         }
+            std::vector<char> depthBuffer(buffer.begin(), buffer.end());
+            imgDepth.SetFilename("Custom/depth/" + std::to_string(timestamp) + ".png");
+            imgDepth.SetImageMatrixFromVector(depthBuffer);
+            imgDepth.Save();
 
-    //         std::string bytesStr(tempBuffer, bytesRead);
+            depthBuffer.clear();
+            buffer.clear();
+            totalBytes = imgDepth.GetSize();
+            std::cout << "Exiting saving loop..." << std::endl;
+        }
 
-    //         if ("terminate" == bytesStr)
-    //         {
-    //             stream.terminate = true;
-    //             std::cout << "## Terminate command received, closing pipe and exiting data stream loop..." << std::endl;
-    //             break;
-    //         }
-    //     }
-    //     SLAM.Shutdown();
-    //     stream.Close();
+        std::string bytesStr(tempBuffer, bytesRead);
 
-    //     float totalElapsedTime = std::accumulate(vElapsedTime.begin(), vElapsedTime.end(), 0);
-    //     std::cout << "\n## Median Tracking Time: " << vElapsedTime[vElapsedTime.size() / 2] << std::endl;
-    //     std::cout << "## Mean Tracking Time: " << totalElapsedTime / vElapsedTime.size() << std::endl;
+        if ("terminate" == bytesStr)
+        {
+            stream.terminate = true;
+            std::cout << "## Terminate command received, closing pipe and exiting data stream loop..." << std::endl;
+            break;
+        }
+    }
+    // SLAM.Shutdown();
+    stream.Close();
 
-    //     std::string filepath = "./output/RGB_D/";
-    //     std::string path = argv[3];
-    //     size_t lastSlashPos = path.find_last_of('/');
-    //     std::string extractedPath = path.substr(12, lastSlashPos - 12);
+    // float totalElapsedTime = std::accumulate(vElapsedTime.begin(), vElapsedTime.end(), 0);
+    // std::cout << "\n## Median Tracking Time: " << vElapsedTime[vElapsedTime.size() / 2] << std::endl;
+    // std::cout << "## Mean Tracking Time: " << totalElapsedTime / vElapsedTime.size() << std::endl;
 
-    //     SLAM.SaveTrajectoryTUM(filepath + extractedPath);
-    //     SLAM.SaveKeyFrameTrajectoryTUM(filepath + extractedPath);
+    // std::string filepath = "./output/RGB_D/";
+    // std::string path = argv[3];
+    // size_t lastSlashPos = path.find_last_of('/');
+    // std::string extractedPath = path.substr(12, lastSlashPos - 12);
+
+    // SLAM.SaveTrajectoryTUM(filepath + extractedPath);
+    // SLAM.SaveKeyFrameTrajectoryTUM(filepath + extractedPath);
 
     return 0;
 }
-
-// /**
-//  * Get the camera resolution from the yaml settings file.
-//  * cv::FileStorage fSettings(argv[2], cv::FileStorage::READ);
-//  * image.SetResolution(fSettings["Camera.width"], fSettings["Camera.height"], fSettings["Camera.channels"]);
-//  */
-
-// // Working save RGB image from desk1 dataset
-
-// // #include <algorithm>
-// // #include <chrono>
-
-// // #include <opencv2/core/core.hpp>
-// // #include <opencv2/opencv.hpp>
-
-// // // #include <System.h>
-
-// // #include "DataStream.h"
-// // #include "PNGHandler.h"
-
-// // int main(int argc, char **argv)
-// // {
-// //     DataStream stream;
-// //     stream.SetFileName("Custom/data_stream");
-// //     stream.SetPermission(0666);
-
-// //     PNGHandler imgRGB, imgDepth;
-// //     imgRGB.SetResolution(640, 480, 3);
-// //     imgDepth.SetResolution(640, 480, 3);
-
-// //     if (!stream.Init())
-// //     {
-// //         std::cout << "*** ERROR *** Terminating program, pipe initialization failed." << std::endl;
-// //         return 1;
-// //     }
-
-// //     stream.OpenReadOnly();
-
-// //     if (stream.fileDescriptor == -1)
-// //     {
-// //         std::cerr << "Failed to open the named pipe." << std::endl;
-// //         return 1;
-// //     }
-
-// //     std::vector<char> buffer;
-// //     size_t totalBytes = imgDepth.GetSize() + imgRGB.GetSize();
-// //     char tempBuffer[totalBytes];
-
-// //     size_t bytesRead;
-
-// //     while (!stream.terminate)
-// //     {
-
-// //         bytesRead = read(stream.fileDescriptor, tempBuffer, totalBytes);
-// //         buffer.insert(buffer.end(), tempBuffer, tempBuffer + bytesRead);
-// //         totalBytes -= bytesRead;
-
-// //         if (0 == totalBytes)
-// //         {
-// //             std::chrono::high_resolution_clock::time_point currentTime = std::chrono::high_resolution_clock::now();
-// //             std::time_t timestamp = std::chrono::duration_cast<std::chrono::milliseconds>(currentTime.time_since_epoch()).count();
-
-// //             std::vector<char> depthBuffer(buffer.begin(), buffer.begin() + imgDepth.GetSize());
-// //             imgDepth.SetFilename("Custom/depth/" + std::to_string(timestamp) + ".png");
-// //             imgDepth.SetData(depthBuffer);
-// //             imgDepth.Save();
-
-// //             std::vector<char> rgbBuffer(buffer.begin() + imgDepth.GetSize(), buffer.end());
-// //             imgRGB.SetFilename("Custom/rgb/" + std::to_string(timestamp) + ".png");
-// //             imgRGB.SetData(rgbBuffer);
-// //             imgRGB.Save();
-
-// //             depthBuffer.clear();
-// //             rgbBuffer.clear();
-// //             buffer.clear();
-// //             totalBytes = imgDepth.GetSize() + imgRGB.GetSize();
-// //         }
-
-// //         std::string bytesStr(tempBuffer, bytesRead);
-
-// //         if ("terminate" == bytesStr)
-// //         {
-// //             stream.terminate = true;
-// //             std::cout << "## Terminate command received, closing pipe and exiting data stream loop..." << std::endl;
-// //             break;
-// //         }
-// //     }
-
-// //     stream.Close();
-
-// //     return 0;
-// // }
-
-// // Working save depth image from desk1 dataset
--- Custom/PNGHandler.cpp
+++ Custom/PNGHandler.cpp
@@ -1,182 +1,118 @@
-// #include "PNGHandler.h"
+#include "PNGHandler.h"
 
-// PNGHandler::PNGHandler()
-// {
-//     this->width = 0;
-//     this->height = 0;
-//     this->channels = 0;
-//     this->filename = "PNGHandler_Output.png";
-// <<<<<<< HEAD
-// <<<<<<< HEAD
-// =======
-//     this->dat.resize(width * height * channels);
-//     this->img = new cv::Mat(this->width, this->height, CV_8UC3);
-// >>>>>>> 9b788acf309b6c08670892108cfcdf168eef2bff
-// =======
-//     this->dat.resize(width * height * channels);
-//     this->img = new cv::Mat(this->width, this->height, CV_8UC3);
-// >>>>>>> 3321a883e5651259f82ccb4e61777865cd3842e7
-// }
-// PNGHandler::PNGHandler(const int wid, const int hei, const int channels)
-// {
-//     this->width = wid;
-//     this->height = hei;
-//     this->channels = channels;
-//     this->filename = "PNGHandler_Output.png";
-// <<<<<<< HEAD
-// <<<<<<< HEAD
-// =======
-//     this->dat.resize(wid * hei * channels);
-//     this->img = new cv::Mat(this->width, this->height, CV_8UC3);
-// >>>>>>> 9b788acf309b6c08670892108cfcdf168eef2bff
-// =======
-//     this->dat.resize(wid * hei * channels);
-//     this->img = new cv::Mat(this->width, this->height, CV_8UC3);
-// >>>>>>> 3321a883e5651259f82ccb4e61777865cd3842e7
-// }
+PNGHandler::PNGHandler()
+{
+    this->width = 0;
+    this->height = 0;
+    this->channels = 0;
+    this->filename = "PNGHandler_Output.png";
+    this->dat.resize(width * height * channels);
+    this->img = new cv::Mat(this->width, this->height, CV_8UC3);
+}
+PNGHandler::PNGHandler(const int wid, const int hei, const int channels)
+{
+    this->width = wid;
+    this->height = hei;
+    this->channels = channels;
+    this->filename = "PNGHandler_Output.png";
+    this->dat.resize(wid * hei * channels);
+    this->img = new cv::Mat(this->width, this->height, CV_8UC3);
+}
 
-// PNGHandler::~PNGHandler()
-// {
-//     CleanUp();
-// }
+PNGHandler::~PNGHandler()
+{
+    CleanUp();
+}
 
-// void PNGHandler::CleanUp()
-// {
-// <<<<<<< HEAD
-// <<<<<<< HEAD
-//     if (nullptr != img)
-//     {
-//         delete[] img;
-//     }
-//     img = nullptr;
-// =======
-// =======
-// >>>>>>> 3321a883e5651259f82ccb4e61777865cd3842e7
-//     this->width = 0;
-//     this->height = 0;
-//     this->channels = 0;
-//     this->dat.clear();
-//     this->img = nullptr;
-// <<<<<<< HEAD
-// >>>>>>> 9b788acf309b6c08670892108cfcdf168eef2bff
-// =======
-// >>>>>>> 3321a883e5651259f82ccb4e61777865cd3842e7
-// }
+void PNGHandler::CleanUp()
+{
+    if (nullptr != img)
+    {
+        delete[] img;
+    }
+    img = nullptr;
+    this->width = 0;
+    this->height = 0;
+    this->channels = 0;
+    this->dat.clear();
+    this->img = nullptr;
+}
 
-// void PNGHandler::SetResolution(const int wid, const int hei, const int channels)
-// {
-//     if (1 > wid || 1 > hei || 1 > channels)
-//     {
-//         throw std::invalid_argument("*** Error *** Image width and height must be greater than 0 pixel.");
-//     }
-//     CleanUp();
-//     this->width = wid;
-//     this->height = hei;
-//     this->channels = channels;
-// <<<<<<< HEAD
-// <<<<<<< HEAD
-// =======
-//     this->dat.resize(wid * hei * channels);
-//     this->img = new cv::Mat(this->width, this->height, CV_8UC3);
-// >>>>>>> 9b788acf309b6c08670892108cfcdf168eef2bff
-// =======
-//     this->dat.resize(wid * hei * channels);
-//     this->img = new cv::Mat(this->width, this->height, CV_8UC3);
-// >>>>>>> 3321a883e5651259f82ccb4e61777865cd3842e7
-// }
+void PNGHandler::SetResolution(const int wid, const int hei, const int channels)
+{
+    if (1 > wid || 1 > hei || 1 > channels)
+    {
+        throw std::invalid_argument("*** Error *** Image width and height must be greater than 0 pixel.");
+    }
+    CleanUp();
+    this->width = wid;
+    this->height = hei;
+    this->channels = channels;
+    this->dat.resize(wid * hei * channels);
+    this->img = new cv::Mat(this->width, this->height, CV_8UC3);
+}
 
-// void PNGHandler::SetImageMatrix(const std::vector<char> &buffer)
-// {
-//     if (!buffer.empty())
-//     {
-//         if (nullptr != this->img)
-//         {
-//             CleanUp();
-//         }
-//         this->img = new Mat(this->width, this->height, CV_8UC3);
-//         std::memcpy(img.data, buffer.data(), buffer.size());
-//     }
-//     else
-//     {
-//         throw std::invalid_argument("*** Error *** Unable to set image from buffer, buffer was empty...");
-//     }
-// }
+void PNGHandler::SetImageMatrixFromVector(const std::vector<char> &imgBuffer)
+{
+    if (!imgBuffer.empty())
+    {
+        std::copy(imgBuffer.begin(), imgBuffer.end(), this->img->data);
+    }
+}
 
-// void PNGHandler::SetImageMatrixFromVector(const std::vector<char> &imgBuffer)
-// {
-//     if (!imgBuffer.empty())
-//     {
-//         std::copy(imgBuffer.begin(), imgBuffer.end(), img->data);
-//     }
-// }
+void PNGHandler::SetFilename(const std::string &fn)
+{
+    if (fn != "" && fn != this->filename)
+    {
+        this->filename = fn;
+    }
+}
 
-// void PNGHandler::SetFilename(const std::string &fn)
-// {
-//     if (fn != "" && fn != this->filename)
-//     {
-//         this->filename = fn;
-//     }
-// }
+int PNGHandler::GetSize(void) const
+{
+    return this->width * this->height * this->channels;
+}
 
-// int PNGHandler::GetSize(void) const
-// {
-//     return this->width * this->height * this->channels;
-// }
+cv::Mat PNGHandler::GetImageMatrix(void) const
+{
+    return *this->img;
+}
 
-// <<<<<<< HEAD
-// <<<<<<< HEAD
-// bool PNGHandler::Save() const
-// =======
-// =======
-// >>>>>>> 3321a883e5651259f82ccb4e61777865cd3842e7
-// cv::Mat PNGHandler::GetImageMatrix(void) const
-// {
-//     return *img;
-// }
+bool PNGHandler::Save() const
+{
+    if (0 < this->width && 0 < this->height && 0 < this->channels)
+    {
+        if (nullptr != this->img)
+        {
+            // Convert RGB to BGR
+            cv::Mat bgrImage;
+            cv::cvtColor(*this->img, bgrImage, cv::COLOR_RGB2BGR);
 
-// bool PNGHandler::Save()
-// <<<<<<< HEAD
-// >>>>>>> 9b788acf309b6c08670892108cfcdf168eef2bff
-// {
-//     if (0 < this->width && 0 < this->height && 0 < this->channels)
-//     {
-// <<<<<<< HEAD
-//         if (nullptr != this->img)
-//         {
-//             // Convert RGB to BGR
-//             cv::Mat bgrImage;
-//             cv::cvtColor(this->img, bgrImage, cv::COLOR_RGB2BGR);
+            cv::imwrite(this->filename, bgrImage);
 
-//             cv::imwrite(this->filename, bgrImage);
+            std::cout << "Image saved with filename: " << this->filename << std::endl;
+            return true;
+        }
+        {
+            if (0 < this->width && 0 < this->height && 0 < this->channels)
+            {
+                cv::Mat image(this->height, this->width, CV_8U);
+                std::memcpy(image.data, dat.data(), dat.size());
 
-//             std::cout << "Image saved with filename: " << this->filename << std::endl;
-//             return true;
-//         }
-// =======
-// =======
-// {
-//     if (0 < this->width && 0 < this->height && 0 < this->channels)
-//     {
-// >>>>>>> 3321a883e5651259f82ccb4e61777865cd3842e7
-//         cv::Mat image(this->height, this->width, CV_8U);
-//         std::memcpy(image.data, dat.data(), dat.size());
+                // Convert RGB to BGR
+                // cv::Mat bgrImage;
+                // cv::cvtColor(image, bgrImage, cv::COLOR_RGB2BGR);
 
-//         // Convert RGB to BGR
-//         // cv::Mat bgrImage;
-//         // cv::cvtColor(image, bgrImage, cv::COLOR_RGB2BGR);
+                cv::imwrite(this->filename, image);
 
-//         cv::imwrite(this->filename, image);
-
-//         std::cout << "Image saved with filename: " << this->filename << std::endl;
-//         return true;
-// <<<<<<< HEAD
-// >>>>>>> 9b788acf309b6c08670892108cfcdf168eef2bff
-// =======
-// >>>>>>> 3321a883e5651259f82ccb4e61777865cd3842e7
-//     }
-//     else
-//     {
-//         throw std::invalid_argument("*** Error *** Image resolution must be greater than 1x1 pixel. Current image resolution is " + std::to_string(this->width) + " x " + std::to_string(this->height) + ". You can change the image resolution by calling SetResolution()");
-//         return false;
-//     }
-// }
+                std::cout << "Image saved with filename: " << this->filename << std::endl;
+                return true;
+            }
+            else
+            {
+                throw std::invalid_argument("*** Error *** Image resolution must be greater than 1x1 pixel. Current image resolution is " + std::to_string(this->width) + " x " + std::to_string(this->height) + ". You can change the image resolution by calling SetResolution()");
+                return false;
+            }
+        }
+    }
+}
